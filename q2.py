34f46fdf8aa1b27ab1a13d7a763b0d05e96f26eb
Kyu Lee <kwooler@gmail.com>
2022-09-25 21:43:11 -0700

    q2

--- q2.py
+++ q2.py
@@ -8,3 +8,21 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+
+cnt = 0
+while True:
+    str = input()
+    if (str.lower() == 'stop'):
+        break
+    if cnt == 0:
+        minlen = maxlen = len(str)
+        maxstr = minstr = str
+    if minlen > len(str):
+        minstr = str
+        minlen = len(str)
+    if maxlen < len(str):
+        maxstr = str
+        maxlen = len(str)
+    cnt += 1
+
+print(maxstr, minstr)
